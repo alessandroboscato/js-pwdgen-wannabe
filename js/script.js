cf5fa44e5da3cebb1d6ab87f3b47f9f21362ad60
Alessandro <alessandro.boscato@gmail.com>
2020-08-03 15:56:25 +0200

    Inserite variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert('Hello World!');
+alert('Rispondi a qualche semplice domanda e ricevi subito la tua password personalizzata e infallibile')
+
+// Chiedi all’utente il suo nome,
+
+var nomeUtente = prompt("Qual'è il tuo nome?");
+console.log (nomeUtente);
+// poi chiedi il suo cognome,
+var cognomeUtente = prompt("Qual'è il tuo cognome?");
+console.log (cognomeUtente);
+// poi chiedi il suo colore preferito
+var colorePreferito = prompt("Qual'è il tuo colore preferito?");
+console.log (colorePreferito);
+// Infine scrivi sulla pagina nomecognomecolorepreferito19
